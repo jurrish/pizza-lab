eedb447e56da3f252b89fde30a91d765bb2623c6
Frank Iriarte <jurrish@gmail.com>
2016-03-23 13:00:05 -0700

    added more table info

--- app.js
+++ app.js
@@ -1,50 +1,50 @@
 console.log('pizza shit');
 
-//create pizzaDemand Function
-function pizzaDemand(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-//create deliveryDemand Function
-function deliveryDemand(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-//create driverDemand Function
-function driverDemand(deliveryDemand) {
-  return Math.ceil(deliveryDemand / 3);
-}
+// //create pizzaDemand Function
+// function pizzaDemand(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
 //
-// create timeArrays for each store that have demand functions at a given time(.push into time array)
+// //create deliveryDemand Function
+// function deliveryDemand(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
 //
-// // storesArray = [ballard, firstHill, internationalDistrict, southLakeUnion, georgetown, ravenna];
-// function createLocation(name){
-//   var storeLocation = {
-//     name : name,
-//     hourlyData : [],
-//   };
-//
-//   function getHourlyData(time, minDelivery, maxDelivery, minPizza, maxPizza) {
-//     var am8 = {
-//       time: time,
-//       pizzasSold: pizzaDemand(minPizza, maxPizza),
-//       deliveriesMade: deliveryDemand(minDelivery, maxDelivery),
-//     };
-//
-//     am8.driversRecommended = driverDemand(am8.deliveriesMade);
-//
-//     storeLocation.hourlyData.push(am8);
-//   }
-//
-//   getHourlyData('8:00 am', 0, 4, 0,4);
-//   getHourlyData('9:00 am', 0, 4, 0,4);
-//   getHourlyData('11:00 am', 0, 4, 0,4);
-//   return storeLocation;
-// };
-//
-// var ballard = createLocation('ballard');
-// var freemont = createLocation('freemont');
-//
+// //create driverDemand Function
+// function driverDemand(deliveryDemand) {
+//   return Math.ceil(deliveryDemand / 3);
+// }
+// //
+// // create timeArrays for each store that have demand functions at a given time(.push into time array)
+// //
+// // // storesArray = [ballard, firstHill, internationalDistrict, southLakeUnion, , ravenna];
+// // function createLocation(name){
+// //   var storeLocation = {
+// //     name : name,
+// //     hourlyData : [],
+// //   };
+// //
+// //   function getHourlyData(time, minDelivery, maxDelivery, minPizza, maxPizza) {
+// //     var am8 = {
+// //       time: time,
+// //       pizzasSold: pizzaDemand(minPizza, maxPizza),
+// //       deliveriesMade: deliveryDemand(minDelivery, maxDelivery),
+// //     };
+// //
+// //     am8.driversRecommended = driverDemand(am8.deliveriesMade);
+// //
+// //     storeLocation.hourlyData.push(am8);
+// //   }
+// //
+// //   getHourlyData('8:00 am', 0, 4, 0,4);
+// //   getHourlyData('9:00 am', 0, 4, 0,4);
+// //   getHourlyData('11:00 am', 0, 4, 0,4);
+// //   return storeLocation;
+// // };
+// //
+// // var ballard = createLocation('ballard');
+// // var freemont = createLocation('freemont');
+// //
 
 function getRandomIntInclusive(max, min) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -86,3 +86,45 @@ ballard.pushHourlyData(new HourlyData('11:00 pm', 1, 3, 4, 12));
 ballard.pushHourlyData(new HourlyData('12:00 am', 8, 15, 6, 16));
 ballard.pushHourlyData(new HourlyData('1:00 am', 8, 15, 6, 16));
 ballard.pushHourlyData(new HourlyData('2:00 am', 8, 15, 6, 16));
+
+var firstHill = new PizzaLocation('First Hill');
+firstHill.pushHourlyData((new HourlyData('8:00 am', 1, 3, 1, 7)));
+firstHill.pushHourlyData((new HourlyData('9:00 am', 1, 3, 1, 7)));
+firstHill.pushHourlyData((new HourlyData('10:00 am', 1, 3, 1, 7)));
+firstHill.pushHourlyData((new HourlyData('11:00 am', 1, 3, 1, 7)));
+firstHill.pushHourlyData((new HourlyData('12:00 pm', 5, 9, 2, 8)));
+firstHill.pushHourlyData((new HourlyData('1:00 pm', 5, 9, 2, 8)));
+firstHill.pushHourlyData((new HourlyData('2:00 pm', 5, 9, 2, 8)));
+firstHill.pushHourlyData((new HourlyData('3:00 pm', 2, 13, 1, 6)));
+firstHill.pushHourlyData((new HourlyData('4:00 pm', 2, 13, 1, 6)));
+firstHill.pushHourlyData((new HourlyData('5:00 pm', 2, 13, 1, 6)));
+firstHill.pushHourlyData((new HourlyData('6:00 pm', 18, 32, 3, 9)));
+firstHill.pushHourlyData((new HourlyData('7:00 pm', 18, 32, 3, 9)));
+firstHill.pushHourlyData((new HourlyData('8:00 pm', 18, 32, 3, 9)));
+firstHill.pushHourlyData((new HourlyData('9:00 pm', 1, 3, 5, 12)));
+firstHill.pushHourlyData((new HourlyData('10:00 pm', 1, 3, 5, 12)));
+firstHill.pushHourlyData((new HourlyData('11:00 pm', 1, 3, 5, 12)));
+firstHill.pushHourlyData((new HourlyData('12:00 am', 8, 20, 6, 16)));
+firstHill.pushHourlyData((new HourlyData('1:00 am', 8, 20, 6, 16)));
+firstHill.pushHourlyData((new HourlyData('2:00 am', 8, 20, 6, 16)));
+
+var iDistrict = new PizzaLocation('The International District');
+iDistrict.pushHourlyData((new HourlyData('8:00 am', 0, 4, 0, 4)));
+iDistrict.pushHourlyData((new HourlyData('9:00 am', 0, 4, 0, 4)));
+iDistrict.pushHourlyData((new HourlyData('10:00 am', 0, 4, 0, 4)));
+iDistrict.pushHourlyData((new HourlyData('11:00 am', 0, 4, 0, 4)));
+iDistrict.pushHourlyData((new HourlyData('12:00 pm', 0, 7, 0, 4)));
+iDistrict.pushHourlyData((new HourlyData('1:00 pm', 0, 7, 0, 4)));
+iDistrict.pushHourlyData((new HourlyData('2:00 pm', 0, 7, 0, 4)));
+iDistrict.pushHourlyData((new HourlyData('3:00 pm', 2, 15, 1, 4)));
+iDistrict.pushHourlyData((new HourlyData('4:00 pm', 2, 15, 1, 4)));
+iDistrict.pushHourlyData((new HourlyData('5:00 pm', 2, 15, 1, 4)));
+iDistrict.pushHourlyData((new HourlyData('6:00 pm', 10, 26, 4, 6)));
+iDistrict.pushHourlyData((new HourlyData('7:00 pm', 10, 26, 4, 6)));
+iDistrict.pushHourlyData((new HourlyData('8:00 pm', 10, 26, 4, 6)));
+iDistrict.pushHourlyData((new HourlyData('9:00 pm', 8, 22, 7, 15)));
+iDistrict.pushHourlyData((new HourlyData('10:00 pm', 8, 22, 7, 15)));
+iDistrict.pushHourlyData((new HourlyData('11:00 pm', 8, 22, 7, 15)));
+iDistrict.pushHourlyData((new HourlyData('12:00 am', 0, 2, 2, 8)));
+iDistrict.pushHourlyData((new HourlyData('1:00 am', 0, 2, 2, 8)));
+iDistrict.pushHourlyData((new HourlyData('2:00 am', 0, 2, 2, 8)));
